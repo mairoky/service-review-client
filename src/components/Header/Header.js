847550c54490a4ee5b01d846dd7d34580bf2c83f
Aminul Islam Rocky <mairoky@outlook.com>
2022-11-08 16:40:31 +0600

    header added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,48 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h2>Header</h2>
-        </div>
+        <header className='py-3'>
+            {['lg'].map((expand) => (
+                <Navbar key={expand} expand={expand}>
+                    <Container>
+                        <Navbar.Brand as={Link} to="/">
+                            EMIKO
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+                        <Navbar.Offcanvas
+                            id={`offcanvasNavbar-expand-${expand}`}
+                            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                            placement="end"
+                        >
+                            <Offcanvas.Header closeButton>
+                                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                                    EMIKO
+                                </Offcanvas.Title>
+                            </Offcanvas.Header>
+                            <Offcanvas.Body>
+                                <Nav className="justify-content-center flex-grow-1 pe-3">
+                                    <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+                                    <Nav.Link as={NavLink} to="/services">Services</Nav.Link>
+                                    <Nav.Link as={NavLink} to="/blog">Blog</Nav.Link>
+                                </Nav>
+                                <Nav>
+                                    <Nav.Link href="#">More</Nav.Link>
+                                </Nav>
+                            </Offcanvas.Body>
+                        </Navbar.Offcanvas>
+                    </Container>
+                </Navbar>
+            ))}
+        </header>
     );
 };
 
